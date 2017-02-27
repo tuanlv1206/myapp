ce27cd58efd58c4d0e96936063b1523e1bd58ee2
tuanmobilefolk <tuan@mobilefolk.com>
2017-02-28 00:08:00 +0700

    add migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(name: "clothes", price: 10)
+Product.create(name: "Shirt", price: 100)
